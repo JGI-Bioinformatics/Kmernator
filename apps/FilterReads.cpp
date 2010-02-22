bd0b3b01b17d19b3d381d473000ba4940dfead8e
regan <regan>
2010-02-22 14:41:37 +0000

    checpoint

--- apps/FilterReads.cpp
+++ apps/FilterReads.cpp
@@ -1,4 +1,4 @@
-// $Header: /repository/PI_annex/robsandbox/KoMer/apps/FilterReads.cpp,v 1.4 2010-01-16 01:12:45 regan Exp $
+// $Header: /repository/PI_annex/robsandbox/KoMer/apps/FilterReads.cpp,v 1.5 2010-02-22 14:41:37 regan Exp $
 //
 
 #include <iostream>
@@ -18,13 +18,24 @@ typedef TrackingDataMinimal8 DataType;
 typedef KmerSpectrum<DataType,DataType> KS;
 typedef ReadSelector<DataType> RS;
 
+// TODO add outputformat of fasta
+// TODO min-read-length 'FULL'
 class FilterReadsOptions : Options
 {
 public:
+    static int getMaxKmerDepth() { return getVarMap()["max-kmer-depth"].as< unsigned int >(); }
+    static int getPartitionByDepth() { return getVarMap()["partition-by-depth"].as< unsigned int > (); }
 	static bool parseOpts(int argc, char *argv[])
 	{
+		// set options specific to this program
 		getPosDesc().add("kmer-size", 1);
         getPosDesc().add("input-file", -1);
+        
+        getDesc().add_options()
+        ("max-kmer-depth",     po::value< unsigned int >()->default_value(0), "maximum number of times a kmer will be represented among the selected reads (mutually exclusive with partition-by-depth)")
+        ("partition-by-depth", po::value< unsigned int >()->default_value(1), "partition filtered reads by powers-of-two coverage depth (mutually exclusive with max-kmer-depth)" )
+        ;
+        
 		return Options::parseOpts(argc, argv);
 	}
 };
@@ -68,24 +79,49 @@ int main(int argc, char *argv[]) {
 
     cerr << "Picking reads: " << endl;
     RS selector(reads, spectrum.weak, Options::getMinDepth());
+    
     long oldPicked = 0;
     long picked = 0;
-    for(unsigned int depth = 4096 ; depth >= 2; depth /= 2) {
-      if (reads.hasPairs())
-        picked = selector.pickAllPassingPairs(depth);
-      else
-        picked = selector.pickAllPassingReads(depth);
-      cerr << "At or above coverage: " << depth << " Picked " << picked << " / " << reads.getSize() << " reads" << endl;
     
-      std::string outputFile = Options::getOutputFile();
-      if (picked > 0 && !outputFile.empty()) {
-        outputFile += "-" + boost::lexical_cast<std::string>( depth );
-    	cerr << "Writing reads to output file: " << outputFile << endl;
-    	ofstream out;
-    	out.open(outputFile.c_str());
-    	selector.writePicks(out, oldPicked);
-    	out.close();
-      }
-      oldPicked += picked;
+    int maximumKmerDepth = FilterReadsOptions::getMaxKmerDepth();
+    
+    if (maximumKmerDepth > 0) {
+    	for(int depth = 1 ; depth < maximumKmerDepth ; depth++) {
+    		cerr << "Picking depth " << depth << " layer of reads" << endl;
+    		if (reads.hasPairs())
+    		    picked += selector.pickBestCoveringSubsetPairs(depth, Options::getMinDepth(), Options::getMinReadLength());
+    		else
+    		    picked += selector.pickBestCoveringSubsetReads(depth, Options::getMinDepth(), Options::getMinReadLength());
+    	}
+    	std::string outputFile = Options::getOutputFile();
+	    if (picked > 0 && !outputFile.empty()) {
+	    	cerr << "Writing reads to output file: " << outputFile << endl;
+	    	ofstream out;
+	    	out.open(outputFile.c_str());
+	    	selector.writePicks(out, oldPicked);
+	    	out.close();
+	    }
+    } else {
+    
+        unsigned int maxDepth = FilterReadsOptions::getPartitionByDepth();
+
+	    for(unsigned int depth = maxDepth ; depth >= 1; depth /= 2) {
+	      if (reads.hasPairs())
+	        picked = selector.pickAllPassingPairs(depth, Options::getMinReadLength());
+	      else
+	        picked = selector.pickAllPassingReads(depth, Options::getMinReadLength());
+	      cerr << "At or above coverage: " << depth << " Picked " << picked << " / " << reads.getSize() << " reads" << endl;
+	    
+	      std::string outputFile = Options::getOutputFile();
+	      if (picked > 0 && !outputFile.empty()) {
+	        outputFile += "-" + boost::lexical_cast<std::string>( depth );
+	    	cerr << "Writing reads to output file: " << outputFile << endl;
+	    	ofstream out;
+	    	out.open(outputFile.c_str());
+	    	selector.writePicks(out, oldPicked);
+	    	out.close();
+	      }
+	      oldPicked += picked;
+	    }
     }
 }
