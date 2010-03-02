807ccfe65e739362b0aada886e552387af9cc8c5
regan <regan>
2010-03-02 14:27:00 +0000

    reformatted

--- src/TwoBitSequence.cpp
+++ src/TwoBitSequence.cpp
@@ -1,4 +1,4 @@
-// $Header: /repository/PI_annex/robsandbox/KoMer/src/TwoBitSequence.cpp,v 1.18 2010-02-26 13:01:16 regan Exp $
+// $Header: /repository/PI_annex/robsandbox/KoMer/src/TwoBitSequence.cpp,v 1.19 2010-03-02 14:27:00 regan Exp $
 //
 
 #include <cstring>
@@ -160,86 +160,90 @@ void TwoBitSequence::shiftLeft(const void *twoBitIn, void *twoBitOut,
 		return;
 	}
 
-	if (shiftAmountInBases > 3)
+	if (shiftAmountInBases > 3) {
 		throw ;
-
-		in+= twoBitLength;
-		out += twoBitLength;
-
-		unsigned short buffer;
-		if (hasExtraByte)
-		buffer = *((unsigned short *)--in);
-		else
-		buffer = *(--in);
-
-		const int shift = (8-shiftAmountInBases*2);
-		for (SequenceLengthType i = 0; i < twoBitLength; i++) {
-			TwoBitEncoding byte = ((buffer >> 8) | (buffer << 8)) >> shift;
-			if (i < twoBitLength -1)
-			buffer = *((unsigned short *)--in);
-			*--out = byte;
-		}
-
 	}
 
-	//
-	// $Log: TwoBitSequence.cpp,v $
-	// Revision 1.18  2010-02-26 13:01:16  regan
-	// reformatted
-	//
-	// Revision 1.17  2010-01-13 23:34:59  regan
-	// made const class modifications
-	//
-	// Revision 1.16  2009-12-24 00:55:57  regan
-	// made const iterators
-	// fixed some namespace issues
-	// added support to output trimmed reads
-	//
-	// Revision 1.15  2009-11-06 16:59:11  regan
-	// added base substitution/permutations table and build function
-	//
-	// Revision 1.14  2009-11-02 18:24:29  regan
-	// *** empty log message ***
-	//
-	// Revision 1.13  2009-10-31 00:16:35  regan
-	// minor changes and optimizations
-	//
-	// Revision 1.12  2009-10-27 22:13:41  cfurman
-	// removed bit shift table
-	//
-	// Revision 1.11  2009-10-26 17:38:43  regan
-	// moved KmerSizer to Kmer.h
-	//
-	// Revision 1.10  2009-10-23 23:22:41  regan
-	// checkpoint
-	//
-	// Revision 1.9  2009-10-23 01:24:53  cfurman
-	// ReadSet test created
-	//
-	// Revision 1.8  2009-10-23 00:13:54  cfurman
-	// reverse complement now works
-	//
-	// Revision 1.7  2009-10-22 21:46:49  regan
-	// fixed ushort to ulong conversion problems
-	//
-	// Revision 1.6  2009-10-22 20:49:15  cfurman
-	// tests added
-	//
-	// Revision 1.5  2009-10-22 07:04:06  regan
-	// added a few unit tests
-	// minor refactor
-	//
-	// Revision 1.4  2009-10-22 01:39:43  cfurman
-	// bug fix in kmer.h
-	//
-	// Revision 1.3  2009-10-22 00:07:43  cfurman
-	// more kmer related classes added
-	//
-	// Revision 1.2  2009-10-21 18:44:20  regan
-	// checkpoint
-	//
-	// Revision 1.1  2009-10-21 06:51:34  regan
-	// bug fixes
-	// build lookup tables for twobitsequence
-	//
-	//
+	in+= twoBitLength;
+	out += twoBitLength;
+
+	unsigned short buffer;
+	if (hasExtraByte)
+	buffer = *((unsigned short *)--in);
+	else
+	buffer = *(--in);
+
+	const int shift = (8-shiftAmountInBases*2);
+	for (SequenceLengthType i = 0; i < twoBitLength; i++) {
+		TwoBitEncoding byte = ((buffer >> 8) | (buffer << 8)) >> shift;
+		if (i < twoBitLength -1)
+		buffer = *((unsigned short *)--in);
+		*--out = byte;
+	}
+
+}
+
+//
+// $Log: TwoBitSequence.cpp,v $
+// Revision 1.19  2010-03-02 14:27:00  regan
+// reformatted
+//
+// Revision 1.18  2010-02-26 13:01:16  regan
+// reformatted
+//
+// Revision 1.17  2010-01-13 23:34:59  regan
+// made const class modifications
+//
+// Revision 1.16  2009-12-24 00:55:57  regan
+// made const iterators
+// fixed some namespace issues
+// added support to output trimmed reads
+//
+// Revision 1.15  2009-11-06 16:59:11  regan
+// added base substitution/permutations table and build function
+//
+// Revision 1.14  2009-11-02 18:24:29  regan
+// *** empty log message ***
+//
+// Revision 1.13  2009-10-31 00:16:35  regan
+// minor changes and optimizations
+//
+// Revision 1.12  2009-10-27 22:13:41  cfurman
+// removed bit shift table
+//
+// Revision 1.11  2009-10-26 17:38:43  regan
+// moved KmerSizer to Kmer.h
+//
+// Revision 1.10  2009-10-23 23:22:41  regan
+// checkpoint
+//
+// Revision 1.9  2009-10-23 01:24:53  cfurman
+// ReadSet test created
+//
+// Revision 1.8  2009-10-23 00:13:54  cfurman
+// reverse complement now works
+//
+// Revision 1.7  2009-10-22 21:46:49  regan
+// fixed ushort to ulong conversion problems
+//
+// Revision 1.6  2009-10-22 20:49:15  cfurman
+// tests added
+//
+// Revision 1.5  2009-10-22 07:04:06  regan
+// added a few unit tests
+// minor refactor
+//
+// Revision 1.4  2009-10-22 01:39:43  cfurman
+// bug fix in kmer.h
+//
+// Revision 1.3  2009-10-22 00:07:43  cfurman
+// more kmer related classes added
+//
+// Revision 1.2  2009-10-21 18:44:20  regan
+// checkpoint
+//
+// Revision 1.1  2009-10-21 06:51:34  regan
+// bug fixes
+// build lookup tables for twobitsequence
+//
+//
