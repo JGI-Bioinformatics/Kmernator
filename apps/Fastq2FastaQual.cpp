0b83c003856e0e996f47e78f7208ab15b5b2de89
regan <regan>
2010-04-14 15:58:34 +0000

    fixed and added fastq2fastaqual

--- apps/Fastq2FastaQual.cpp
+++ apps/Fastq2FastaQual.cpp
@@ -1,4 +1,4 @@
-// $Header: /repository/PI_annex/robsandbox/KoMer/apps/Fastq2Fasta.cpp,v 1.2 2010-04-14 15:58:34 regan Exp $
+// $Header: /repository/PI_annex/robsandbox/KoMer/apps/Fastq2FastaQual.cpp,v 1.1 2010-04-14 15:58:34 regan Exp $
 //
 
 #include <iostream>
@@ -12,20 +12,18 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	if (argc <= 1)
-	    throw std::invalid_argument("Please specify one or more input files.\nFasta will be output to STDOUT");
+	    throw std::invalid_argument("Please specify one or more input files.\nFasta will be output to STDOUT and quals will be output to STDERR");
 
 	ReadSet reads;
 	for(int i = 1 ; i < argc; i++) {
-	   cerr << "Reading Input Files" << endl;
 	   reads.appendAnyFile(std::string(argv[i]));
 	}
-	cerr << "loaded " << reads.getSize() << " Reads, " << reads.getBaseCount()
-		<< " Bases " << endl;
 
 	for(ReadSet::ReadSetSizeType idx = 0 ; idx < reads.getSize(); idx++) {
 		const Read read = reads.getRead(idx);
 
 		cout << ">" << read.getName() << endl << read.getFasta() << endl;
+		cerr << ">" << read.getName() << endl << read.getFormattedQuals() << endl;
 	}
 
 }
