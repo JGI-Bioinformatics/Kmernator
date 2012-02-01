db1e795212ace5b5d46d9ac535d0857b58be6629
Rob Egan <regan@regan2.local>
2012-01-31 22:28:17 -0800

    added R script

---
+++ src/EstimateSize.R
@@ -0,0 +1,25 @@
+args <- commandArgs(TRUE)
+
+file <- args[1]
+
+data <- read.table(file, header = TRUE, sep="\t")
+rawKmers <- data[3]
+uniqueKmers <- data[2]
+d <- data.frame(rawKmers=rawKmers, uniqueKmers=uniqueKmers)
+
+fun3 <- function(x, ax, bx) {
+  val <- ax * x + bx - bx * (( ax + bx - 1) / bx ) ^ x
+  cx <- (( bx + ax - 1) / bx)
+  dy.da <- ( ( -bx * ( cx ) ^ x + bx + ax - 1) * x ) / (bx + ax - 1)
+  dy.db <- ( (ax - 1) * ( cx ) ^ x * x + ( - bx - ax + 1) * ( cx ) ^ x + bx + ax - 1) / ( bx + ax - 1)
+  attr(val, "gradient") <- cbind( ax = dy.da, bx = dy.db)
+  val
+}
+
+cont <- nls.control(maxiter=500, tol=1e-5, minFactor=1/1024,warnOnly=TRUE)
+res <- nls(uniqueKmers ~ fun3(rawKmers, errorRate, genomeSize), d , trace=TRUE, control=cont, start=list(errorRate=0.01, genomeSize=5000000), algorithm='port', lower=list(errorRate=0.0001, genomeSize=500), upper=list(errorRate=0.05, genomeSize=100000000000))
+
+print(summary(res))
+
+res
+
