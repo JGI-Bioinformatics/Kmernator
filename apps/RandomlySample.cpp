5420f1f34cc08a62a59d856c13e72a61b267c507
Rob Egan <regan@genepool02.nersc.gov>
2012-08-31 00:02:53 -0700

    refined blocking algorithm for RandomlySample

--- apps/RandomlySample.cpp
+++ apps/RandomlySample.cpp
@@ -170,7 +170,7 @@ long pickByBlock(ReadFileReader &rfr, long numSamples) {
 	long numBlocks = std::min((long) 100, numSamples / 5);
 	if (numBlocks < 1)
 		numBlocks = 1;
-	long numPicksPerBlock = ((numSamples+1) / numBlocks);
+	long numPicksPerBlock = ((numSamples+numBlocks-1) / numBlocks);
 	long count = 0;
 
 	LOG_DEBUG(1, "pickByBlock: " << numSamples << " blocks: " << numBlocks << " picksPerBlock: " << numPicksPerBlock);
@@ -191,6 +191,7 @@ long pickByBlock(ReadFileReader &rfr, long numSamples) {
 		if (byPair)
 			reads.identifyPairs();
 		long numRead = byPair ? reads.getPairSize() : reads.getSize();
+                numPicksPerBlock = ( (numSamples+numBlocks-block-1) / (numBlocks - block));
 		long numPicks = std::min(numPicksPerBlock, numSamples);
 		numPicks = std::min(numRead, numPicks);
 		LOG_DEBUG(1, "Read " << numRead << (byPair?" pairs" : " reads") << " selecting " << numPicks << " remaining: " << numSamples);
