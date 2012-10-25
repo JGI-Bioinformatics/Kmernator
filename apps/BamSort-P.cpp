2c0c0bcfdd376e70bb128cd92b9653742362e2f3
Rob Egan <regan@dakar.jgi-psf.org>
2012-10-24 17:41:58 -0700

    added unmapped reads option to BamSort-P

--- apps/BamSort-P.cpp
+++ apps/BamSort-P.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <string>
+
 #include "mpi.h"
 #include "SamUtils.h"
 #include "Log.h"
@@ -26,9 +27,20 @@ public:
 	std::string &getUnmappedReadPairs() {
 		return unmappedReadPairs;
 	}
+	std::string &getOutputBam() {
+		return outputBam;
+	}
+	FileListType &getInputBams() {
+		return inputBams;
+	}
 	void _setOptions(po::options_description &desc, po::positional_options_description &p) {
-		po::options_description opts("BamSort-P Options");
+		p.add("output-bam", 1);
+		p.add("input-bams", -1);
+
+		po::options_description opts("BamSort-P <options> output.bam input.bam [...]\n\nOptions");
 		opts.add_options()
+				("output-bam", po::value<std::string>())
+				("input-bams", po::value<FileListType>())
 				("unmapped-read-pairs", po::value<std::string>()->default_value(unmappedReadPairs), "gzipped file to place unmapped read Pairs Fastqs (can be same as --unmapped-reads)")
 				("unmapped-reads", po::value<std::string>()->default_value(unmappedReads), "gzipped file to place unmapped reads Fastqs (can be same as --unmapped-read-pairs)");
 		desc.add(opts);
@@ -38,10 +50,21 @@ public:
 		bool ret = true;
 		setOpt("unmapped-read-pairs", unmappedReadPairs);
 		setOpt("unmapped-reads", unmappedReads);
+		setOpt("output-bam", outputBam);
+		setOpt2("input-bams", inputBams);
+
+		if (outputBam.empty())
+			setOptionsErrorMsg("You must specify at least the outputBam");
+
+		if (inputBams.empty())
+			setOptionsErrorMsg("You must specify at least one input bam");
+
 		// Other ret &= *::_parseOptions(vm);
 		return ret;
 	}
 private:
+	std::string outputBam;
+	FileListType inputBams;
 	std::string unmappedReads;
 	std::string unmappedReadPairs;
 };
@@ -54,20 +77,16 @@ int main(int argc, char **argv)
 	int rank,size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	if (argc <= 2) {
-		if (rank == 0)
-			std::cerr << "Usage: mpirun BamSort-P out.bam [in.[sb]am ...]\n\n";
-		MPI_Finalize();
-		exit(1);
-	}
 
-	GeneralOptions::getOptions().getDebug() = 0;
+	GeneralOptions::getOptions().getDebug() = 1;
 	BamVector reads;
-	std::string outputBam(argv[1]);
+	std::string outputBam = BamSortOptions::getOptions().getOutputBam();
+	OptionsBaseInterface::FileListType inputBams = BamSortOptions::getOptions().getInputBams();
+
 	samfile_t *fh = NULL;
-	for(int i = 2; i < argc; i++) {
-		if ((i-2) % size == rank) {
-			std::string inputFile(argv[i]);
+	for(int i = 0; i < (int) inputBams.size(); i++) {
+		if ((i) % size == rank) {
+			std::string inputFile(inputBams[i]);
 			LOG_VERBOSE_OPTIONAL(1, true, "Reading " << inputFile);
 			if (fh != NULL)
 				BamStreamUtils::closeSamOrBam(fh);
@@ -77,6 +96,29 @@ int main(int argc, char **argv)
 			LOG_VERBOSE_OPTIONAL(1, true, "Loaded " << (reads.size() - s) << " new bam records");
 		}
 	}
+
+	bool needsCollapse = false;
+	if (!BamSortOptions::getOptions().getUnmappedReadPairs().empty()) {
+		BamVector unmappedReads;
+		SamUtils::splitUnmapped(reads, unmappedReads, true);
+		if (!unmappedReads.empty()) {
+			needsCollapse = true;
+			SamUtils::writeFastqGz(world, unmappedReads, BamSortOptions::getOptions().getUnmappedReadPairs(), true);
+			assert(unmappedReads.empty());
+		}
+	}
+	if (!BamSortOptions::getOptions().getUnmappedReads().empty()) {
+		BamVector unmappedReads;
+		SamUtils::splitUnmapped(reads, unmappedReads, false);
+		if (!unmappedReads.empty()) {
+			needsCollapse = true;
+			SamUtils::writeFastqGz(world, unmappedReads, BamSortOptions::getOptions().getUnmappedReads(), true);
+			assert(unmappedReads.empty());
+		}
+	}
+
+	if (needsCollapse)
+		SamUtils::collapseVector(reads);
 
 	unlink(outputBam.c_str());
 	LOG_VERBOSE(1, "Sorting myreads: " << reads.size());
