9e0a70af4b060a56e9e40eecb22b2aca2d30ee2b
Rob Egan <regan@dakar.jgi-psf.org>
2012-11-26 17:14:38 -0800

    bugfixes and fixed option handling

--- apps/MergeIndexPartitionedBam-P.cpp
+++ apps/MergeIndexPartitionedBam-P.cpp
@@ -12,40 +12,88 @@
 #include "SamUtils.h"
 #include "Log.h"
 #include "Options.h"
+#include "MPIUtils.h"
 
+class _MergeIndexPartitionedBamOptions : public OptionsBaseInterface {
+public:
+	void _resetDefaults() {
+		GeneralOptions::_resetDefaults();
+		GeneralOptions::getOptions().getDebug() = 2;
+		GeneralOptions::getOptions().getMaxThreads() = 1;
+	}
+	void _setOptions(po::options_description &desc, po::positional_options_description &p) {
+		p.add("output-bam", 1);
+		p.add("num-partitions", 1);
+		p.add("input-bams", -1);
+
+		po::options_description opts("MergeIndexPartitionedBam-P <options> output.bam num_partitions input.bam [...]\n\nOptions");
+		opts.add_options()
+				("output-bam", po::value<std::string>())
+				("input-bams", po::value<FileListType>())
+				("num-partitions", po::value<int>()->default_value(0), "The number of index partitions");
+		desc.add(opts);
+
+		GeneralOptions::_setOptions(desc, p);
+		// Other *::_setOptions(desc,p);
+	}
+	bool _parseOptions(po::variables_map &vm) {
+		bool ret = true;
+
+		ret |= GeneralOptions::_parseOptions(vm);
+
+		setOpt("output-bam", outputBam);
+		setOpt2("input-bams", inputBams);
+		setOpt("num-partitions", numPartitions);
+
+		if (outputBam.empty())
+			setOptionsErrorMsg("You must specify at least the outputBam");
+
+		if (inputBams.empty())
+			setOptionsErrorMsg("You must specify at least one input bam");
+
+		// Other ret &= *::_parseOptions(vm);
+		return ret;
+	}
+	std::string outputBam;
+	FileListType inputBams;
+	int numPartitions;
+};
+typedef OptionsBaseTemplate< _MergeIndexPartitionedBamOptions > MergeIndexPartitionedBamOptions;
 int main(int argc, char **argv)
 {
 
-	if (MPI_SUCCESS != MPI_Init(&argc, &argv))
-		LOG_THROW("MPI_Init() failed: ");
 
-	int rank,size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	if (argc != 2 + size) {
+	ScopedMPIComm< MergeIndexPartitionedBamOptions > world(argc, argv);
+
+	int rank = world.rank(), size = world.size();
+
+	std::string ourOutputBam = MergeIndexPartitionedBamOptions::getOptions().outputBam;
+	int partitions = MergeIndexPartitionedBamOptions::getOptions().numPartitions;
+	std::string myInputFile = MergeIndexPartitionedBamOptions::getOptions().inputBams[rank];
+
+	if (size != (int) MergeIndexPartitionedBamOptions::getOptions().inputBams.size()) {
 		if (rank == 0)
-			std::cerr << "Usage: mpirun MergeIndexPartitionedBam-P out.bam [in1.[sb]am in2.[sb]am ...]\n\tYour run size must be equal to the number of inputs\n\n";
-		MPI_Finalize();
+			std::cerr << " Your run size must be equal to the number of inputs\n " << MergeIndexPartitionedBamOptions::getDesc() << std::endl;
 		exit(1);
 	}
-	mpi::communicator world;
-	Logger::setWorld(&world);
-	GeneralOptions::getOptions().getDebug() = 2;
 
-	std::string ourOutputBam(argv[1]);
-	std::string myInputFile(argv[2+rank]);
+	if (partitions == 0 || size % partitions != 0) {
+		if (rank == 0)
+			std::cerr << "The partitions " << partitions << " is not a factor of the size " << size << MergeIndexPartitionedBamOptions::getDesc() << std::endl;
+		exit(1);
+	}
 	BamVector myReads;
 	{
-		SamUtils::MPIMergeSam mergeSam(MPI_COMM_WORLD, myInputFile, myReads);
+		int color = rank / partitions;
+		mpi::communicator partitionWorld = world.split(color);
+		LOG_VERBOSE(1, "Input: " << myInputFile << " split color: " << color << " rank: " << partitionWorld.rank() << " of " << partitionWorld.size());
+		SamUtils::MPIMergeSam mergeSam(partitionWorld, myInputFile, myReads);
 		unlink(ourOutputBam.c_str());
 		mergeSam.outputMergedHeader(ourOutputBam);
-		SamUtils::MPISortBam sortBam(MPI_COMM_WORLD, myReads, ourOutputBam, NULL);
+		SamUtils::MPISortBam sortBam(world, myReads, ourOutputBam, NULL);
 	}
 
 	LOG_VERBOSE(1, "Finished");
 
-	Logger::setWorld(NULL);
-	if (MPI_SUCCESS != MPI_Finalize())
-		LOG_THROW("MPI_Finalize() failed: ");
 	return 0;
 }
