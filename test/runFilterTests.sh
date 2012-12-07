10a996a4b3b72f11b27740be1d29d055c726ec06
Rob Egan <gpint01.nersc.gov>
2012-12-06 20:31:38 -0800

    fixed tests for default ReadSelectorOptions change

--- test/runFilterTests.sh
+++ test/runFilterTests.sh
@@ -23,20 +23,19 @@ GOOD=1000-Filtered.fastq
 
 check()
 {
-  echo "Executing: $@ --mask-simple-repeats 0 --artifact-edit-distance 1 --out $TMP 31 $IN"
-  if $@ --mask-simple-repeats 0 --artifact-edit-distance 1 --out $TMP 31 $IN
+  opts=" --kmer-scoring-type MEDIAN --mask-simple-repeats 0 --artifact-edit-distance 1 --out $TMP 31 $IN"
+  echo "Executing: $@ $opts"
+  if $@ $opts
   then
-    if ! diff -q $TMP-MinDepth2-1000.fastq $GOOD
+    if ! diff -w -q $TMP-MinDepth2-1000.fastq $GOOD
     then
        echo "FAILED $@ --out $TMP 31 $IN"
        wc $TMP-MinDepth2-1000.fastq $GOOD
-       diff $TMP-MinDepth2-1000.fastq $GOOD | head -50
-       rm -f $TMP*
+       diff -w $TMP-MinDepth2-1000.fastq $GOOD | head -50
        exit 1
     fi
   else
     echo "FAILED with exit status $?: $@ --out $TMP 31 $IN"
-    rm -f $TMP*
     exit 1
   fi
 }
@@ -45,6 +44,7 @@ GOOD=1000-Filtered-readlength.fastq
 check $FR --min-read-length 1 
 GOOD=1000-Filtered-readlength-both.fastq
 check $FR --min-read-length 1 --min-passing-in-pair 2
+rm -f $TMP*
 
 
 IN=1000.fastq
@@ -52,11 +52,11 @@ GOOD=1000-Filtered.fastq
 
 for thread in {1..3}
 do
-  check $FR --thread $thread
+  check $FR --min-read-length 25 --thread $thread
   rm -f $TMP*
-  check $FR --thread $thread --save-kmer-mmap 1
+  check $FR --min-read-length 25 --thread $thread --save-kmer-mmap 1
   mv $TMP-mmap $TMP-mmap-saved
-  check $FR --thread $thread --load-kmer-mmap $TMP-mmap-saved
+  check $FR --min-read-length 25 --thread $thread --load-kmer-mmap $TMP-mmap-saved
   rm -f $TMP*
 done
 
@@ -79,18 +79,18 @@ then
       break
     fi
     export OMP_NUM_THREADS=$(((procs+mpi-1)/mpi))
-    check $MPI $mpi $FRP --thread 1
+    check $MPI $mpi $FRP --min-read-length 25 --thread 1
     rm -f $TMP*
-    check $MPI $mpi $FRP
+    check $MPI $mpi $FRP --min-read-length 25
     rm -f $TMP*
-    check $MPI $mpi $FRP --thread 1 --save-kmer-mmap 1
+    check $MPI $mpi $FRP --min-read-length 25 --thread 1 --save-kmer-mmap 1
     mv $TMP-mmap $TMP-mmap-saved
-    check $MPI $mpi $FRP --thread 1 --load-kmer-mmap $TMP-mmap-saved
-    check $FR --load-kmer-mmap $TMP-mmap-saved
+    check $MPI $mpi $FRP --min-read-length 25 --thread 1 --load-kmer-mmap $TMP-mmap-saved
+    check $FR --min-read-length 25 --load-kmer-mmap $TMP-mmap-saved
     rm -f $TMP*
-    check $FR --save-kmer-mmap 1 
+    check $FR --min-read-length 25 --save-kmer-mmap 1 
     mv $TMP-mmap $TMP-mmap-saved
-    check $MPI $mpi $FRP --load-kmer-mmap $TMP-mmap-saved
+    check $MPI $mpi $FRP --min-read-length 25 --load-kmer-mmap $TMP-mmap-saved
     rm -f $TMP*
   done
 fi
