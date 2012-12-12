678aeefefdc10812060852da58ff0513848d55b2
Rob Egan <regan@dakar.jgi-psf.org>
2012-12-12 14:14:25 -0800

    removed old file

--- make_version.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-my $out = shift;
-my $ver = <>;
-chomp $ver;
-my $exists = `grep $ver $out`;
-if ($exists !~ /$ver/) {
-    open(my $fh, ">$out") || die;
-    print $fh "#define VERSION \"$ver\"\n";
-}
