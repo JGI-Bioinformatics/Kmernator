2a24c7925c42c8bb5b218e57d15b23591c361a76
Rob Egan <regan@dakar.jgi-psf.org>
2013-01-14 10:58:38 -0800

    check for numa support before invoking OpenMPI bind-to-socket or bind-to-core options

--- test/runMeraculousTests.sh
+++ test/runMeraculousTests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 MPI=""
+MPI_OPTS=""
 procs=$(grep -c ^processor /proc/cpuinfo)
 
 true=$(which true)
 
 mt=time
-if memtime $true
+if memtime $true 2>/dev/null
 then
   mt=memtime
 fi
@@ -14,11 +15,18 @@ fi
 ismpi=0
 if mpirun $true
 then
-  MPI="mpirun -bycore -bind-to-core -np"
+  MPI="mpirun"
+  if $MPI -bycore -bind-to-core $true
+  then
+    MPI_OPTS="-bycore -bind-to-core -np"
+  else
+    MPI_OPTS="-np"
+  fi
   ismpi=1
 elif aprun -n 1 $true
 then
-  MPI="aprun -n"
+  MPI="aprun"
+  MPI_OPTS="-n"
   procs=$(aprun -B -q uname -n | wc -l)
 fi
 
@@ -88,12 +96,14 @@ then
       break
     fi
     export OMP_NUM_THREADS=1
-     check $mt $MPI $mpi $FRP
+     check $mt $MPI $MPI_OPTS $mpi $FRP
   done
   
-  if ((ismpi)) && mpirun -n $procs -bysocket -bind-to-socket $true
+  if ((ismpi)) && $MPI -bysocket -bind-to-socket $true
   then
-   MPI="mpirun -bysocket -np"
+   MPI_OPTS="-bysocket -bind-to-socket -np"
+  else
+   MPI_OPTS="-np"
   fi
    
   echo "Running in hybrid mode"
@@ -114,7 +124,7 @@ then
     fi
 
     export OMP_NUM_THREADS=$threads
-    check $mt $MPI $mpi $socket $FRP
+    check $mt $MPI $MPI_OPTS $mpi $socket $FRP
   done
 
 fi
